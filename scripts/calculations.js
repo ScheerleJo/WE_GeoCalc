e13c886d8d429055234f074ef97322dba1bac683
Josia <scheerle.josia@gmail.com>
2024-07-13 11:59:06 +0200

    Fix jump from formula to calculator and add comments

--- scripts/calculations.js
+++ scripts/calculations.js
@@ -1,13 +1,35 @@
+/**
+ * This script provides the functionality to calculate the area or volume of different shapes.
+ */
 
+
+/**
+ * Adds an event listener to the dropdown menu to show the calculation form based on the selected shape.
+ * Sets the default value of the dropdown menu to 'rectangle' and shows the calculation form for a rectangle.
+ * Calls the function to show the calculation form.
+ * Tries to retrieve the selected shape from the local storage and shows the form if it exists.
+ * @see showForm
+*/
 function onCalcBodyLoad() {
-    document.getElementById('geo-options').addEventListener('change', showForm);
-    document.getElementById('geo-options').value = 'rectangle';
-    showCalcRectangle();
+    document.getElementById('geo-options').addEventListener('change', ()=> showForm());
+    let data = localStorage.getItem('setSelected');
+    if (data){
+        data = JSON.parse(data).selected;
+        localStorage.removeItem('setSelected');
+        showForm(data);
+        document.getElementById('geo-options').value = data;
+    } else {
+        showForm('rectangle');
+        document.getElementById('geo-options').value = 'rectangle';
+    }
 }
 
-
-function showForm() {
-    const shape = document.getElementById('geo-options').value;
+/**
+ * Shows the calculation form based on the selected shape in the dropdown menu.
+ * @param {string} geometry optional parameter to set the shape specifically
+*/
+function showForm(geometry = undefined) {
+    const shape = geometry || document.getElementById('geo-options').value;
     switch (shape) {
         case 'rectangle': showCalcRectangle(); break;
         case 'square': showCalcSquare(); break;
@@ -21,7 +43,11 @@ function showForm() {
     }
 }
 
-
+/**
+ * Calculates the area or volume of a shape based on the given values.
+ * The calculation is based on the selected shape in the dropdown menu.
+ * The function will calculate the missing value if one of the values is missing.
+ */
 function calculate() {
 
     const shape = document.getElementById('geo-options').value;
@@ -34,50 +60,53 @@ function calculate() {
         c = parseFloat(document.getElementById('input-c').value);
     } catch (error) {
         console.error('Error: ' + error);
-        return;
+        return; // Exit the function if an error occured while parsing the values
     }
     if(a < 0 || b < 0 || c < 0) {
         alert('Bitte geben sie gültige Werte für die Berechnung ein');
-        return;
+        return; // Exit the function if the values are invalid
     }
+    if(isNaN(a)) a = 0;
+    if(isNaN(b)) b = 0;
+    if(isNaN(c)) c = 0;
 
     // Calculate normally if all values are given
     switch(shape) {
         case 'rectangle':
             // If one value is missing, calculate the missing value (Simulate a XOR operation)
-            if(xor(a,b) && result != 0) {
+            if(xor(result, a, b)) {
                 isNormalCalculation = false;
                 if(a == 0) a = result / b;
-                else a = result / a;
+                else b = result / a;
             } else result = a * b;
             break;
         case 'square':
-            if(a == 0 && result != 0) {
+            if(xor(result, a)) {
                 isNormalCalculation = false;
                 a = Math.sqrt(result);
             } else result = Math.pow(a, 2);
             break;
         case 'triangle':
-            if(xor(a, b) && result != 0) {
+            if(xor(result, a, b)) {
                 isNormalCalculation = false;
                 if(a == 0) a = 2 * result / b;
                 else b = 2 * result / a;
             } else result = a * b * 0.5;
             break;
         case 'circle':
-            if(a == 0 && result != 0) {
+            if(xor(result, a)) {
                 isNormalCalculation = false;
                 a = Math.sqrt(result / Math.PI);
             } else result = Math.PI * Math.pow(a, 2);
             break;
         case 'cube':
-            if(a == 0 && result != 0) {
+            if(xor(result, a)) {
                 isNormalCalculation = false;
                 a = Math.cbrt(result);
             } else result = Math.pow(a, 3);
             break;
         case 'cuboid':
-            if(xor(a,b,c) && result != 0) {
+            if(xor(result, a, b, c)) {
                 isNormalCalculation = false;
                 if(a == 0) a = result / (b * c);
                 else if(b == 0) b = result / (a * c);
@@ -85,13 +114,13 @@ function calculate() {
             } else result = a * b * c;
             break;
         case 'sphere':
-            if(a == 0 && result != 0) {
+            if(xor(result, a)) {
                 isNormalCalculation = false;
                 a = Math.cbrt((3/4) * (result / Math.PI));
             } else result = ((4/3) * Math.PI * Math.pow(a, 3));
             break;
         case 'pyramid':
-            if(xor(a,b,c) && result != 0) {
+            if(xor(result, a, b, c)) {
                 isNormalCalculation = false;
                 if(a == 0) a = (3 * result) / (b * c);
                 else if(b == 0) b = (3 * result) / (a * c);
@@ -99,7 +128,7 @@ function calculate() {
             } else result = ((1/3) * (a * b) * c);
             break;
         case 'tetraeder':
-            if(xor(a,b,c) && result != 0) {
+            if(xor(result, a, b, c)) {
                 isNormalCalculation = false;
                 if(a == 0) a = (3 * result) / (b * c);
                 else if(b == 0) b = (3 * result) / (a * c);
@@ -115,13 +144,19 @@ function calculate() {
     }
 }
 
-
+/**
+ * Shows the Input-Boxes to calculate the area of a square
+ */
 function showCalcSquare() {
     showSpecificInputs(true);
     document.getElementById('label-a').innerText = 'Seitenlänge a';
     document.getElementById('formula-text').innerHTML = '<math xmlns = "http://www.w3.org/1998/Math/MathML"><mi>A</mi><mo>=</mo><mi>a</mi><mo>&middot</mo><mi>a</mi></math>';
     document.getElementById('label-result').innerText = 'Ergebnnis A';
 }
+
+/**
+ * Shows the Input-Boxes to calculate the area of a rectangle
+*/
 function showCalcRectangle() {
     showSpecificInputs(true, true);
     document.getElementById('label-a').innerText = 'Seitenlänge a';
@@ -129,6 +164,10 @@ function showCalcRectangle() {
     document.getElementById('formula-text').innerHTML = '<math xmlns = "http://www.w3.org/1998/Math/MathML"><mi>A</mi><mo>=</mo><mi>a</mi><mo>&middot</mo><mi>b</mi></math>';
     document.getElementById('label-result').innerText = 'Ergebnnis A';
 }
+
+/**
+ * Shows the Input-Boxes to calculate the area of a triangle
+ */
 function showCalcTriangle() {
     showSpecificInputs(true, true);
     document.getElementById('label-a').innerText = 'Grundseite a';
@@ -136,18 +175,30 @@ function showCalcTriangle() {
     document.getElementById('formula-text').innerHTML = '<math xmlns = "http://www.w3.org/1998/Math/MathML"><mi>A</mi><mo>=</mo><mfrac><mn>1</mn><mn>2</mn></mfrac><mo>&middot</mo><mi>a</mi><mo>&middot</mo><mi>h</mi></math>';
     document.getElementById('label-result').innerText = 'Ergebnnis A';
 }
+
+/**
+ * Shows the Input-Boxes to calculate the area of a circle
+*/
 function showCalcCircle() {
     showSpecificInputs(true);
     document.getElementById('label-a').innerText = 'Radius r';
     document.getElementById('formula-text').innerHTML = '<math xmlns = "http://www.w3.org/1998/Math/MathML"><mi>A</mi><mo>=</mo><mi>π</mi><mo>&middot</mo><msup><mi>r</mi><mn>2</mn></msup></math>';
     document.getElementById('label-result').innerText = 'Ergebnnis A';
 }
+
+/**
+ * Shows the Input-Boxes to calculate the volume of a cube
+ */
 function showCalcCube() {
     showSpecificInputs(true);
     document.getElementById('label-a').innerText = 'Kantenlänge a';
     document.getElementById('formula-text').innerHTML = '<math xmlns = "http://www.w3.org/1998/Math/MathML"><mi>V</mi><mo>=</mo><msup><mi>a</mi><mn>3</mn></msup></math>';
     document.getElementById('label-result').innerText = 'Ergebnnis V';
 }
+
+/**
+ * Shows the Input-Boxes to calculate the volume of a cuboid
+ */
 function showCalcCuboid() {
     showSpecificInputs(true, true, true);
     document.getElementById('label-a').innerText = 'Kantenlänge a';
@@ -156,11 +207,19 @@ function showCalcCuboid() {
     document.getElementById('formula-text').innerHTML = '<math xmlns = "http://www.w3.org/1998/Math/MathML"><mi>V</mi><mo>=</mo><mi>a</mi><mo>&middot</mo><mi>b</mi><mo>&middot</mo><mi>c</mi></math>';
     document.getElementById('label-result').innerText = 'Ergebnnis V';
 }
+
+/**
+ * Shows the Input-Boxes to calculate the volume of a sphere
+ */
 function showCalcSphere() {
     showSpecificInputs(true);
     document.getElementById('label-a').innerText = 'Radius r';
     document.getElementById('formula-text').innerHTML = '<math xmlns = "http://www.w3.org/1998/Math/MathML"><mi>V</mi><mo>=</mo><mfrac><mn>4</mn><mn>3</mn></mfrac><mo>&middot</mo><mi>π</mi><mo>&middot</mo><msup><mi>r</mi><mn>3</mn></msup></math>';
 }
+
+/**
+ * Shows the Input-Boxes to calculate the volume of a pyramid
+ */
 function showCalcPyramid() {
     showSpecificInputs(true, true, true);
     document.getElementById('label-a').innerText = 'Grundseite a';
@@ -169,7 +228,9 @@ function showCalcPyramid() {
     document.getElementById('formula-text').innerHTML = '<math xmlns = "http://www.w3.org/1998/Math/MathML"><mi>V</mi><mo>=</mo><mfrac><mn>1</mn><mn>3</mn></mfrac><mo>&middot</mo><mi>a</mi><mo>&middot</mo><mi>b</mi><mo>&middot</mo><mi>h</mi></math>';
     document.getElementById('label-result').innerText = 'Ergebnnis V';
 }
-
+/**
+ * Shows the Input-Boxes to calculate the volume of a tetraeder
+ */
 function showCalcTetraeder() {
     showSpecificInputs(true, true, true);
     document.getElementById('label-a').innerText = 'Kante Grundfläche a';
@@ -179,16 +240,23 @@ function showCalcTetraeder() {
     document.getElementById('label-result').innerText = 'Ergebnnis V';
 }
 
+/**
+ * Shows the specific inputs for the calculation and hides the others.
+ * Parameters are optional and default to false to hide all inputs. If true, the input will be shown.
+ * @param {boolean} input1 true if input 1 should be visible
+ * @param {boolean} input2 true if input 2 should be visible
+ * @param {boolean} input3 true if input 3 should be visible
+ */
 function showSpecificInputs(input1 = false,input2 = false,input3 = false) {
     let a = document.getElementById('input-a');
     let b = document.getElementById('input-b');
     let c = document.getElementById('input-c');
     a.style.display = input1? 'block' : 'none';
-    a.value = input1? 0: '';
+    a.value = '';
     b.style.display = input2? 'block' : 'none';
-    b.value = input2? 0: '';
+    b.value = '';
     c.style.display = input3? 'block' : 'none';
-    c.value = input3? 0: '';
+    c.value = '';
     
     document.getElementById('label-a').style.display = input1? 'block' : 'none';
     document.getElementById('label-b').style.display = input2? 'block' : 'none';
@@ -197,11 +265,22 @@ function showSpecificInputs(input1 = false,input2 = false,input3 = false) {
     document.getElementById('result').value = '';
 }
 
-function xor(a = undefined, b = undefined, c = undefined) {
-    let xor// = (a === 0 && b !== 0 && c !== 0) || (a !== 0 && b === 0 && c !== 0) || (a !== 0 && b !== 0 && c === 0);
-    xor = (isEmpty(a) && !isEmpty(b) && !isEmpty(c)) || (!isEmpty(a) && isEmpty(b) && !isEmpty(c)) || (!isEmpty(a) && !isEmpty(b) && isEmpty(c));
-    return xor;
+/**
+ * Adds XOR functionality to JS.
+ * @param {number} a input a
+ * @param {number} b input b
+ * @param {number} c input c
+ * @param {number} result input result
+ * @returns {boolean} true if one value is missing and the result is given
+ */
+function xor(result = undefined, a = undefined, b = undefined, c = undefined) {
+    return ((isEmpty(a) && !isEmpty(b) && !isEmpty(c) && !isEmpty(result)) || (!isEmpty(a) && isEmpty(b) && !isEmpty(c) && !isEmpty(result)) || (!isEmpty(a) && !isEmpty(b) && isEmpty(c) && !isEmpty(result)));
 }
+/**
+ * Adds isEmpty functionality to JS. Checks if a value is 0 or an empty string.
+ * @param {*} value 
+ * @returns {boolean} true if the value is 0 or an empty string
+ */
 function isEmpty(value) {
     return value === 0 || value === '';
 }
--- scripts/showInformation.js
+++ scripts/showInformation.js
@@ -1,3 +1,7 @@
+/**
+ * Expands the page and shows all information of the selected geometry
+ * @param  {String} geometry - The geometry to show information of
+ */
 function showInformation(geometry) {
     let section = document.getElementById('info-' + geometry);
     hideEverythingElse(geometry);
@@ -6,6 +10,11 @@ function showInformation(geometry) {
     document.getElementById('button-' + geometry).classList.add('active-button');
     document.getElementById('reset-highlights').style.display = 'block';
 }
+
+/**
+ * Hides all information of every figure except the selected geometry
+ * @param  {string} geometry - The geometry to to still be visible
+ */
 function hideEverythingElse(geometry) {
     let sections = document.getElementsByClassName('info');
     for (let i = 0; i < sections.length; i++) {
@@ -21,7 +30,9 @@ function hideEverythingElse(geometry) {
         }
     }
 }
-
+/**
+ * Hides all borders of the information sections in ```formulas.html```
+ */
 function hideBorders(){
     let sections = document.getElementsByClassName('info');
     for (let i = 0; i < sections.length; i++) {
@@ -37,15 +48,23 @@ function hideBorders(){
     document.getElementById('reset-highlights').style.display = 'none';
 }
 
+/**
+ * Highlights the 2D formulas in ```formulas.html``` with a border around the article
+ */
 function highlight2D (){
     document.getElementById('formulas-2d').classList.add('highlight');
 }
+/**
+ * Highlights the 3D formulas in ```formulas.html``` with a border around the article
+ */
 function highlight3D (){
     document.getElementById('formulas-3d').classList.add('highlight');
 }
 
 
-
+/**
+ * Function is activated when Body is loaded. It hides all information sections and starts Listening on KeyDown events
+ */
 function loadBody(){
     hideEverythingElse('none');
     listenOnKeyDown();
@@ -53,21 +72,28 @@ function loadBody(){
 }
 
 
+/**
+ * Function listens to KeyDown events and triggers when the 'k' key is pressed.
+ */
 function listenOnKeyDown() {
     console.log('Listening to keydown events');
     document.body.addEventListener('keydown', function(event) {
         if(event.key === 'k' || event.key === 'K') {
             hideEverythingButSphere();
+            // Add a button to restore/relaod the page
             if(document.getElementById('formula-buttons').childElementCount === 1) {
                 button = document.createElement('button');
-                button.appendChild(document.createTextNode('Restore'));
-                button.addEventListener('click', restoreEverything);
+                button.appendChild(document.createTextNode('Wiederherstellen'));
+                button.addEventListener('click', () => window.location.reload());
                 document.getElementById('formula-buttons').appendChild(button);
             }
         }
     });
 }
 
+/**
+ * Deletes all information sections except the sphere and shows the information of the sphere 
+ */
 function hideEverythingButSphere() {
     console.log("Delete everything.... You better run!");;
     document.querySelectorAll("section.info, button.info-button").forEach(element => {if(!element.id.includes('-sphere')) element.remove()});
@@ -76,4 +102,14 @@ function hideEverythingButSphere() {
     showInformation('sphere');
 }
 
-function restoreEverything() { window.location.reload();}
+/**
+ * Function to show the calculator page with speficied geometry
+ * @param {String} geometry - The geometry to show the calculator of
+ */
+function showCalc(geometry) {
+    let data = {
+        selected: geometry,
+    }
+    localStorage.setItem('setSelected', JSON.stringify(data));
+    window.location.href = 'calculator.html';
+}
